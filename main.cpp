8ed8aaa8b14fd3d2d1c241820eff40920d58759d
LiuYiFan <2311889551@qq.com>
2023-09-06 23:41:44 +0800

    01.创建一个窗口

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
  */
 
 /**
- * @类型<a href="https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/ns-winuser-wndclassa">WNDCLASS</a>每个窗口都必须与一个窗口类相关联，即使程序曾经只创建该类的一个实例。 窗口类不是 C++ 意义上的类。 相反，它是操作系统内部使用的数据结构。 窗口类在运行时向系统注册。
+ * @类型 <a href="https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/ns-winuser-wndclassa">WNDCLASS</a>: 每个窗口都必须与一个窗口类相关联，即使程序曾经只创建该类的一个实例。 窗口类不是 C++ 意义上的类。 相反，它是操作系统内部使用的数据结构。 窗口类在运行时向系统注册。
+ * \n
+ * @类型 HINSTANCE: 实例的句柄 或模块的句柄.当可执行文件加载到内存中时，操作系统使用此值来标识可执行文件或 EXE。 某些 Windows 函数需要实例句柄，例如加载图标或位图。
+ * \n
+ * @类型 HWND: Windows是对象,它们同时具有代码和数据，但它们不是 C++ 类。 相反，程序通过使用称为 句柄的值来引用窗口。 句柄是不透明的类型。 从本质上讲，它只是操作系统用来标识对象的数字。 你可以将 Windows 想象成有一个大表，其中包含已创建的所有窗口。 它使用此表按句柄查找窗口。
  */
 
  /**
@@ -11,6 +15,13 @@
  * @参数 szCmdLine 字符串的形式包含命令行参数。
  * @参数 nCmdShow 是一个标志，指示主应用程序窗口是最小化、最大化还是正常显示.
  * */
+
+/**
+ * @窗口创建步骤 1.   创建并注册窗口类(WNDCLASS)
+ * @窗口创建步骤 2.   创建窗口(CreateWindow)
+ * @窗口创建步骤 3.   显示窗口(ShowWindow)并写消息处理循环(get -> translate -> dispatch)
+ */
+
 #include <windows.h>
 
  int WINAPI WinMain(HINSTANCE hInstance , HINSTANCE, PSTR szCmdLine , int nCmdShow)
