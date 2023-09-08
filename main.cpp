0a16165bfe322fc0ac29d66882a542958b1b649d
LiuYiFan <2311889551@qq.com>
2023-09-08 12:48:21 +0800

    02.消息系统

--- main.cpp
+++ main.cpp
@@ -27,15 +27,17 @@
 {
     const TCHAR szWndClassName[] = TEXT("Window Class Name");/*每个WNDCLASS都需要一个唯一标识的名字,该名称只需在当前进程中是唯一的。*/
 
-    WNDCLASS wndClass = {};//必须初始化,wndclass除了一下成员都可以初始化为0
+    WNDCLASSEX wndClass = {};//必须初始化,wndclass除了一下成员都可以初始化为0
     wndClass.hInstance = hInstance; //搬运一下当前模块实例
     wndClass.lpfnWndProc = WinProc;//设置窗口消息处理函数
     wndClass.lpszClassName = szWndClassName;//设置wndclass名称
+    wndClass.style = CS_OWNDC;
+    wndClass.cbSize = sizeof(wndClass);
 
-    if(!RegisterClass(&wndClass))
+    if(!RegisterClassEx(&wndClass))
         return 1;
 
-    auto hWnd = CreateWindowEx(0,szWndClassName,TEXT("window name"),WS_OVERLAPPEDWINDOW,0,0,800,600,NULL,NULL,hInstance,0);
+    auto hWnd = CreateWindowEx(0,szWndClassName,TEXT("window name"),WS_SYSMENU|WS_CAPTION|WS_MINIMIZEBOX,0,0,800,600,NULL,NULL,hInstance,0);
     MSG msg;
 
     ShowWindow(hWnd,nCmdShow);
